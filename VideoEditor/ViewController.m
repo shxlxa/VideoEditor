197cdc390d84285f5d977584d9db50229c35a8e8
aoni-chengft <842495841@qq.com>
2017-04-22 09:28:01 +0800

    basic function 开启一个定时器，每0.1s截一次图

--- VideoEditor/ViewController.m
+++ VideoEditor/ViewController.m
@@ -10,12 +10,17 @@
 #import <AVFoundation/AVFoundation.h>
 #import <MediaPlayer/MediaPlayer.h>
 
+#define kImageCount 10
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *thImageView;
 
 @property (nonatomic, strong) NSURL  *myUrl;
+@property (nonatomic, strong) NSTimer  *timer;
+@property (nonatomic, strong) NSMutableArray  *imageArr;
 
-@property (nonatomic, assign) NSInteger cutCount;
+@property (nonatomic, assign) CGFloat cutTime; //截图时间
+@property (nonatomic, assign) NSInteger cutCount; //截图次数
+@property (nonatomic, assign) NSInteger videoDuration;
 
 @end
 
@@ -28,28 +33,49 @@
     NSURL *url = [NSURL fileURLWithPath:str];
     self.myUrl = url;
     
-//    self.myUrl = [NSURL URLWithString:@"https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"];
-    
+    self.cutTime = 0.0;
     self.cutCount = 0;
+    self.videoDuration = [self getVideoDurationWithUrl:url];
 }
 
 - (IBAction)item:(id)sender {
-    
-   
-    self.cutCount += 5;
-    [self assetGetThumImage:self.cutCount];
-    
-//    [self thumbnailImageForVideo:self.myUrl atTime:63.0];
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:0.15 target:self selector:@selector(timerEvent:) userInfo:nil repeats:YES];
+}
+
+- (void)timerEvent:(NSTimer *)timer{
+    if (self.cutCount < kImageCount) {
+        [self assetGetThumImageWithUrl:self.myUrl time:self.cutTime];
+        self.cutCount ++;
+        self.cutTime += self.videoDuration / kImageCount;
+    } else {
+        self.cutTime = 0;
+        self.cutCount = 0;
+        [self.timer invalidate];
+        
+        NSLog(@"imagecount:%ld",self.imageArr.count);
+    }
+}
+
+/**
+ 获取视频总时长
+
+ @param url url
+ @return 视频总时间：秒
+ */
+- (CGFloat)getVideoDurationWithUrl:(NSURL *)url{
+    AVURLAsset *urlSet = [AVURLAsset assetWithURL:url];
+    //获取视频总时间
+    CGFloat totaltime = urlSet.duration.value / urlSet.duration.timescale;
+    return totaltime;
 }
 
 
 // CMTimeMake CMTimeMakeWithSeconds
 //这两个的区别是 CMTimeMake(a,b) a当前第几帧, b每秒钟多少帧.当前播放时间a/b CMTimeMakeWithSeconds(a,b) a当前时间,b每秒钟多少帧.
-- (void)assetGetThumImage:(CGFloat)second
+- (void)assetGetThumImageWithUrl:(NSURL *)url time:(CGFloat)second
 {
     AVURLAsset *urlSet = [AVURLAsset assetWithURL:self.myUrl];
     //获取视频总时间
-    CGFloat totaltime = urlSet.duration.value / urlSet.duration.timescale;
     AVAssetImageGenerator *imageGenerator = [AVAssetImageGenerator assetImageGeneratorWithAsset:urlSet];
     NSError *error = nil;
     CMTime time = CMTimeMakeWithSeconds(second,10);//缩略图创建时间 CMTime是表示电影时间信息的结构体，第一个参数表示是视频第几秒，第二个参数表示每秒帧数.(如果要活的某一秒的第几帧可以使用CMTimeMake方法)
@@ -64,9 +90,23 @@
     UIImageWriteToSavedPhotosAlbum(image,nil, nil,nil);
     CGImageRelease(cgImage);
     self.thImageView.image = image;
+    if (image) {
+        [self.imageArr addObject:image];
+    }
     NSLog(@"视频截取成功");
 }
 
+- (NSMutableArray *)imageArr{
+    if (!_imageArr) {
+        _imageArr = [NSMutableArray array];
+    }
+    return _imageArr;
+}
+
+- (void)dealloc
+{
+    [self.timer invalidate];
+}
 
 
 
